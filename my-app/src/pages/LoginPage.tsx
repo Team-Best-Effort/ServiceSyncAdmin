6a088738623a59a0e7c2360d9e6261d4fa0dcf54
HashemB2 <hbattah@csus.edu>
2025-02-05 19:53:12 -0800

    added temp hashmap

--- my-app/src/pages/LoginPage.tsx
+++ my-app/src/pages/LoginPage.tsx
@@ -1,8 +1,31 @@
 import React from "react"; // Import React for creating components.
 import './CSS/LoginPage.css'; // Import custom CSS for styling.
 import { Link } from 'react-router-dom';
+import CryptoJS from 'crypto-js';
 
 const LoginPage = () => {
+    const userDatabase = new Map<string, string>();
+
+const registerUser = (email: string, password: string) => {
+    if (userDatabase.has(email)) {
+        console.log('Registration Failed: Email already exists');
+    } else {
+        const hashedPassword = CryptoJS.SHA256(password).toString();
+        userDatabase.set(email, hashedPassword);
+        console.log('Registration Successful: User added to database');
+    }
+};
+
+const validateUser = (email: string, password: string) => {
+    const hashedPassword = CryptoJS.SHA256(password).toString();
+    if (userDatabase.has(email) && userDatabase.get(email) === hashedPassword) {
+        console.log('User validated successfully');
+        return true;
+    } else {
+        console.log('User validation failed');
+        return false;
+    }
+};
     return (
         <div className="login-container">
             <div className="login-box">
